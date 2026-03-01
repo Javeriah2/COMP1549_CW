8869ae9408da723a1fa4695f043d7ee43535f122
Javeriah2 <Javeriahwork@gmail.com>
2026-03-01 17:26:28 +0000

    Editing policies

--- src/securityapp/policy/ManagementAccessPolicy.java
+++ src/securityapp/policy/ManagementAccessPolicy.java
@@ -1,5 +1,74 @@
 package securityapp.policy;
 
-public class ManagementAccessPolicy {
+import securityapp.model.Resource;
+import securityapp.model.User;
+import securityapp.model.AccessScope;
+import securityapp.model.Role;
+
+public class ManagementAccessPolicy implements AccessPolicy{
+
+	@Override
+	public boolean canRead(User user, Resource resource) {
+		// TODO Auto-generated method stub
+		if (resource.getScope() == AccessScope.PUBLIC) {
+			return true;
+		}
+		else if (resource.getScope() == AccessScope.INTERNAL) {
+			switch (user.getRole()) {
+			case DEVELOPER:
+			case MANAGER:
+			case SYS_ADMIN:
+				return true;
+			default:
+				return false;
+			}
+		}
+		else if (resource.getScope() == AccessScope.CONFIDENTIAL) {
+			switch (user.getRole()) {
+			case MANAGER:
+			case SYS_ADMIN:
+				return true;
+			default:
+				return false;
+			}
+		}
+		return false;
+	}
+
+	@Override
+	public boolean canWrite(User user, Resource resource) {
+		// TODO Auto-generated method stub
+		if (resource.getScope() == AccessScope.PUBLIC) {
+			switch (user.getRole()) {
+			case INTERN:
+			case SYS_ADMIN:
+				return true;
+			default:
+				return false;
+			}
+		
+		}
+		else if (resource.getScope() == AccessScope.INTERNAL) {
+			switch (user.getRole()) {
+			case DEVELOPER:
+			case MANAGER:
+			case SYS_ADMIN:
+				return true;
+			default:
+				return false;
+			}
+		}
+		else if (resource.getScope() == AccessScope.CONFIDENTIAL) {
+			switch (user.getRole()) {
+			case MANAGER:
+			case SYS_ADMIN:
+				return true;
+			default:
+				return false;
+			}
+		}
+		
+		return false;
+	}
 
 }
