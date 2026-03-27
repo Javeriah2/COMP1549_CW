6891c46f56caed22d64d4a593cc8b9dfd28ed072
Donald724276 <donaldsworkhub@gmail.com>
2026-03-27 12:46:46 +0000

    Changed a bit of text

--- src/securityapp/Main.java
+++ src/securityapp/Main.java
@@ -41,7 +41,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("---INITIALIZE SECURITY SYS---");
+        System.out.println("INITIALIZE SECURITY SYS");
 
         SecurityProxy proxy = new SecurityProxy();
 
